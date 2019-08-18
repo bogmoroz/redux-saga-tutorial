e429d4b5dfcc41c8dd52839c09612ee2cd807975
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-18 23:23:56 +0300

    Named export into default, imported 'All' effect

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import createSagaMiddleware from 'redux-saga';
 import Counter from './Counter';
 import reducer from './reducers';
 
-import { rootSaga } from './sagas';
+import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
